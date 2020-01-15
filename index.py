0c0fe8cd051dc1e0501d5284cac45f5c7f359cc5
jlogan112 <jarvislogan2@gmail.com>
2020-01-15 10:29:32 -0500

    Update and rename index.html to index.py

---
+++ index.py
@@ -0,0 +1,105 @@
+<%TICKET_PRICE = 200
+
+tickets_remaining = 10000
+
+service = 20
+
+
+
+
+def calculate_price(num_tickets):
+  tax = TICKET_PRICE * num_tickets * .07
+  return (TICKET_PRICE * num_tickets) + service + tax
+  
+while tickets_remaining >= 1: 
+  print(""".....\n.....\n.....\nWELCOME!!!!!!!\n.....\n.....\n.....""")
+  print("Tickets are available for the Luke Combs concert!!!!!!!") 
+  print("Tickets are $200 a piece!")
+  print("There will be a 1 time service charge of $20 per order.")
+  print("There are {} tickets remaining.".format(tickets_remaining))
+  name = input("What is your name?  ")
+  name = name.capitalize()
+  num_tickets = input("Hello {}, how many tickets would you like?   ".format(name))
+  try:
+    num_tickets = int(num_tickets)
+    if num_tickets > tickets_remaining:
+      raise ValueError("There are only {} tickets left.".format(tickets_remaining))
+  except ValueError as err:
+      print("Oh no, we ran into an issue. {} Please try again.".format(err))
+  else:
+    price = calculate_price(num_tickets)
+    print("""So for {} tickets at $200/ticket and a 7 percent sales tax.....\n....\nprocessing....\n.....""".format(num_tickets))
+    print("Your total due is ${}.".format(price)) 
+    confirm = input("Would you like to proceed {}? Y?N  ".format(name))
+    if confirm.lower() == "y":
+        tickets_remaining -= num_tickets 
+        card = int(input("please enter a valid card number  "))       
+        while card < 1000000000000001:
+          print("The card you entered is not valid and must be 16 digits!")
+          card = int(input("please enter a valid card number  "))
+        print("Thank you very much!")
+        cname = input("Is your name the on the card? Y?N   ")
+        if cname.lower() == "y":
+          print("ok")
+        else:
+          actname = input("Please enter the name on the card. ")
+          perm = input("Did {} give you permission to use the card? Y?N   " .format(actname))
+          if actname.lower() == "y":
+            print("ok")
+          else:
+            print("we dont really care as long as we get paid")
+        cvv = int(input("please enter a valid '3' digit code "))
+        while cvv < 100:
+          print("The number you entered is not valid")
+          cvv = int(input("please enter a valid '3' digit code "))
+        print("your payment is being processed")
+        print(""".....\n.....\n.....\n.....\n.....\n.....\n.....\n one moment please \n.....\n.....\n.....\n.....\n.....""")
+        print("payment confirmed")
+        ask = input("Is there anything else you would like? Y?N ")
+        if ask.lower() == "y":
+          print("ok")
+          print("......\n.....\n.....\n....")
+          print("Please call Big Willie at 828-DEEZ-NUTS!")
+          print("....\n....\n.... or ....\n....\n....\n email jarvislogan2@gmail.com")
+        else:
+              print("ok")
+        print("Now we would like to know if you want the tickets mailed or emailed.")
+        ship = input("Simply answer with mail (to mail them) or email (to email them).  ")
+        if ship.lower() == "mail":         
+            zip_code = int(input("What is the zip code?  "))
+            street = input("what is the name of the street?  ")
+            street = street.capitalize()
+            street_num = int(input("What is the street number?  "))
+            city = input("What is the city? ")
+            city = city.capitalize()
+            state = input("What is the state?  ")
+            state = state.upper()
+            print("we have the following address for you \n {} {}\n {}, {}  {}.".format(street_num, street, city, state, zip_code))
+            correct_add = input("Is this address correct? Y?N   ")
+            while correct_add != "y":
+              zip_code = int(input("What is the zip code?  "))
+              street = input("what is the name of the street?  ")
+              street = street.capitalize()
+              street_num = int(input("What is the street number?  "))
+              city = input("What is the city? ")
+              city = city.capitalize()
+              state = input("What is the state?  ")
+              state = state.upper()
+              print("we have the following address for you \n {} {}\n {}, {}  {}.".format(street_num, street, city, state, zip_code))
+              correct_add = input("Is this address correct? Y?N   ")
+              
+        else:
+            email = input("Enter email address. ")
+            print("We have the following email address for you \n {}." .format(email))
+            correct_email = input("Is this the correct email? Y?N   ")
+            while correct_email != "y":
+              email = input("Enter email address. ")
+              print("We have the following email address for you \n {}." .format(email))
+              correct_email = input("Is this the correct email? Y?N   ")
+              
+        print("Thank you for your business. Your order has been processed and is being shipped via {}!" .format(ship))
+        print("If by chance you have entered the incorrect {}\n please call our customer support at \n........ 1800-DONT-GIVE-A-F!!!!...........".format(ship))
+    else:
+      print("Thank you anyways.")
+print("Sorry tickets are sold out. :(") 
+%>
